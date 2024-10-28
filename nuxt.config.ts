2a9bda1dfc74151f72f45e21b3ea048e4ba6458d
fanjindai <fanchen4139@163.com>
2024-10-28 14:10:19 +0800

    feat: first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import routerOptions from './router/index'
+import { resolve } from 'path'
+export default defineNuxtConfig({
+  // router: routerOptions,
+  // router: {
+  //   options: {
+  //     routes: routerOptions
+  //   }
+  // },
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, '.')
+  },
+  app: {
+    baseURL: '/nuxt-app/'
+  },
+  vite: {
+    // base: './'
+  }
+})
